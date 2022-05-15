4dedae3a26afb83583816551bcfd9d478126d718
Stefan Witchell <stefanwitchell@gmail.com>
2022-05-15 22:29:40 +0700

    adding cli tool

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -5,6 +5,13 @@ def fruit():
     fruits = ["apple", "cherry", "banana", "pear"]
     return choices(fruits)[0]
 
+def meal(beverage):
+    my_fruit = fruit()
+    print(f"your fruit is {my_fruit} and your beverage is {beverage}")
+    if my_fruit == "cherry":
+        return f"Your meal is a {my_fruit} and {beverage}"
+    return f"Your meal is a steak and {beverage}"
+
 
 # var = 1
 # var = var
